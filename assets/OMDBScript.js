6f3dc5417822792211a2aad07695c49204376de3
Josh <lutz_143@yahoo.com>
2022-12-01 20:30:55 -0500

    add initial javascript

--- assets/OMDBScript.js
+++ assets/OMDBScript.js
@@ -2,13 +2,6 @@ const omdbApiKey = '2e30998b'
 const omdbRoot = 'https://www.omdbapi.com/?type=movie&r=json&y=2018&s=*bum*&apikey='
 const omdbRoot2 = 'https://www.omdbapi.com/?i=tt4701182&apikey='
 
-// examples
-// http://www.omdbapi.com/?y=2020
-// https://www.omdbapi.com/?i=tt3896198&apikey=2e30998b
-// tt4701182
-
-
-
 
 function getDataCall() {
   var requestUrl = omdbRoot + omdbApiKey;
