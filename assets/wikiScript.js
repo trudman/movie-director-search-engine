c318db87294a7198f8c48d9fed43989b35ce6354
KmtHawk <kmthawk@gmail.com>
2022-12-05 12:17:00 -0500

    udpdated wikiScript

--- assets/wikiScript.js
+++ assets/wikiScript.js
@@ -1,15 +1,55 @@
-function wikiApi() {
-    var wikiUrl = "https://en.wikipedia.org/w/api.php?action=query&origin=*&format=json&generator=search&gsrnamespace=0&gsrlimit=5&gsrsearch=" ;
 
-    fetch(wikiUrl)
-    .then(function (response) {
-        console.log(response);
-        return response.json();
-    })
-    .then(function (data) {
-        console.log(data);
-    })
+// Wiki API search root url
+const wikiSearchRoot = "https://en.wikipedia.org/w/api.php?action=query&origin=*&format=json&generator=search&gsrnamespace=0&gsrlimit=5&gsrsearch=";
+
+
+//Identify movie director
+var directorSearch = [];
+
+//Store movie driector by id
+var movieDirector1 = document.getElementById('director-id-1').textContent;
+console.log(movieDirector1)
+var movieDirector2 = document.getElementById('director-id-2').textContent;
+console.log(movieDirector2)
+console.log(wikiSearchRoot + movieDirector1);
+//var movieDirector3 = document.getElementById('director-id-3').textContent;
+//var movieDirector4 = document.getElementById('director-id-4').textContent;
+//var movieDirector5 = document.getElementById('director-id-5').textContent;
+//var movieDirector6 = document.getElementById('director-id-6').textContent;
+
+
+//Capture movie director value on click
+
+movieDirector1.onclick = function(event) {
+    console.log(event);
+    
+};
+
+
+
+function wikiSearchCall() {
+    var requestUrl = wikiSearchRoot + '*' + directorSearch;
+
+    fetch(requestUrl)
+        .then(function (response) {
+            return (response.json());
+        })
+        .then(function (data) {
+            var directoraResults = data;
+
+            displayResults();
+            console.log(data);
+        });
+
+
 
 }
 
-wikiApi();
+function displayResults() {
+    var itemTitle = item.title;
+    var itemSniippet = item.snippet;
+    
+    console.log(itemTitle);
+    console.log(itemSniippet);
+
+}
